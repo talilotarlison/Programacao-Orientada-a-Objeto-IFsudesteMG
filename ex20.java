d5b7d2b0d8f43ac40e758b5a9829745ac390c9e8
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2025-06-13 09:53:05 -0300

    metedo privado e publico

--- ex20.java
+++ ex20.java
@@ -1,11 +1,11 @@
 // Classe base Smartphone
 class Cliente {
-    public String nome;
-    public String endereco;
+    private String nome; //atributo privado
+    private String endereco;
     public String dataNascimento;
     public Double rendaFamiliar;
-    public String cpf;
-    public String telefone;
+    private String cpf;
+    public String telefone;//atributo publico
 
     // Construtor da classe Smartphone
     public Cliente(String nome, String endereco,String dataNascimento,Double rendaFamiliar,String cpf, String telefone) {
@@ -35,28 +35,30 @@ class Cliente {
     public void setNome(String nome) {
         this.nome = nome;
     }
+        // Método para mostrar cpf
+    public String getCpf() {
+        return this.cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
-        // Criando instâncias de clintes
+        // Criando instâncias 
         Cliente cliente1 = new Cliente("Joao", "Sao Paulo - Zona norte", "14/06/1978", 1500.00, "56566998892", "(86)81596-8895");
         Cliente cliente2 = new Cliente("Ze", "Rio Janeiro - Zona norte", "14/06/1978", 1500.00, "8944949499", "(86)81596-8951");
 
-         System.out.println(cliente1.nome);
-         System.out.println(cliente1.endereco);
          System.out.println(cliente1.dataNascimento);
-         System.out.println(cliente1.cpf);
-         System.out.println(cliente1.telefone);
+         System.out.println(cliente1.telefone); //atributo publico consigo acesso direto e modificação
         
-         System.out.println(cliente1.getNome());
-         cliente1.setNome("Mario");
+         System.out.println(cliente1.getNome()); //atributo privado consigo acessar pela metodo interno da class
+         System.out.println(cliente1.getCpf());
+         cliente1.setNome("Mario"); // consigo modificar apenas pelo metodo
          cliente1.exibirInformacoes();
              
-         System.out.println(cliente2.nome);
-         System.out.println(cliente2.endereco);
          System.out.println(cliente2.dataNascimento);
-         System.out.println(cliente2.cpf);
          System.out.println(cliente2.telefone);
 
          cliente2.exibirInformacoes();
